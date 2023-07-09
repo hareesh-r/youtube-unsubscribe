c15ce55da1be420f0d5d04de00037de8f16d4c6a
HAREESH <hareeshprogrammer@gmail.com>
2023-07-09 20:21:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-let counter = 0;
+let batchCounter = 0;
 let timeout = 150;
 
 async function unsubscribeFromAllChannels() {
-  const allSubs = document.querySelectorAll("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
+  const subscriptionButtons = document.querySelectorAll("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
 
   for (let i = 0; i < 101; i++) {
-    allSubs[i].click();
+    subscriptionButtons[i].click();
     await new Promise(resolve => setTimeout(resolve, timeout));
 
-    const temp = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item");
-    temp.click();
+    const unsubscribeOption = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item");
+    if (unsubscribeOption == null) {
+      continue;
+    }
+    unsubscribeOption.click();
     await new Promise(resolve => setTimeout(resolve, timeout));
 
-    const temp1 = document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
-    temp1.click();
+    const confirmButton = document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
+    confirmButton.click();
     await new Promise(resolve => setTimeout(resolve, timeout));
 
     // Scroll one-third of the screen width to the bottom
     window.scrollBy(0, 140);
 
     if (i === 100) {
-      console.log("Completed batch " + counter++);
+      console.log("Completed batch " + batchCounter++);
       unsubscribeFromAllChannels();
     }
   }
