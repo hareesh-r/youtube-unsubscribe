8699cd7bc44571288ee2ddb94391136f1a3077f3
HAREESH <hareeshprogrammer@gmail.com>
2023-07-09 20:18:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,29 @@
-let batchCounter = 0;
+let counter = 0;
 let timeout = 150;
 
 async function unsubscribeFromAllChannels() {
-  const subscriptionButtons = document.querySelectorAll("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
-  
-  // Check if there are any channels left to unsubscribe
-  if (subscriptionButtons.length === 0) {
-    console.log("Unsubscription completed!");
-    return;
-  }
-  
+  const allSubs = document.querySelectorAll("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
+
   for (let i = 0; i < 101; i++) {
-    if (i >= subscriptionButtons.length) {
-      break;
-    }
-    
-    subscriptionButtons[i].click();
+    allSubs[i].click();
     await new Promise(resolve => setTimeout(resolve, timeout));
-    
-    const unsubscribeOption = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item");
-    unsubscribeOption.click();
+
+    const temp = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item");
+    temp.click();
     await new Promise(resolve => setTimeout(resolve, timeout));
-    
-    const confirmButton = document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
-    confirmButton.click();
+
+    const temp1 = document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
+    temp1.click();
     await new Promise(resolve => setTimeout(resolve, timeout));
-    
+
     // Scroll one-third of the screen width to the bottom
     window.scrollBy(0, 140);
-  }
 
-  console.log("Completed batch: " + batchCounter++);
-  
-  // Delay before processing the next batch
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  
-  unsubscribeFromAllChannels();
+    if (i === 100) {
+      console.log("Completed batch " + counter++);
+      unsubscribeFromAllChannels();
+    }
+  }
 }
 
 unsubscribeFromAllChannels();
