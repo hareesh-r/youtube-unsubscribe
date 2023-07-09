b27de0d34f486b2cf28c240863db28c6f72e8d69
HAREESH <hareeshprogrammer@gmail.com>
2023-07-09 20:27:39 +0530

    Update scrollHeight

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let batchCounter = 0;
 let timeout = 150;
+let scrollHeightOfDiv = document.querySelector("#grid-container > ytd-channel-renderer:nth-child(1)");
 
 async function unsubscribeFromAllChannels() {
   const subscriptionButtons = document.querySelectorAll("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
@@ -7,7 +8,7 @@ async function unsubscribeFromAllChannels() {
   for (let i = 0; i < 101; i++) {
     subscriptionButtons[i].click();
     await new Promise(resolve => setTimeout(resolve, timeout));
-
+    
     const unsubscribeOption = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item");
     if (unsubscribeOption == null) {
       continue;
@@ -20,7 +21,7 @@ async function unsubscribeFromAllChannels() {
     await new Promise(resolve => setTimeout(resolve, timeout));
 
     // Scroll one-third of the screen width to the bottom
-    window.scrollBy(0, 140);
+    window.scrollBy(0, scrollHeightOfDiv.clientHeight);
 
     if (i === 100) {
       console.log("Completed batch " + batchCounter++);
