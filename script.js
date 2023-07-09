93a8a42b2c26e971aeb0e2afaf8ef1130b7c3c52
HAREESH <hareeshprogrammer@gmail.com>
2023-07-09 22:10:31 +0530

    feat : prompt to keep the window open

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ async function executeUnsubscribeCode() {
 
 async function startUnsubscribeProcess() {
   const estimatedTime = numIterations * timeout / 1000; // Calculate the estimated time in seconds
-  const confirmationMessage = `The process will start in approximately ${estimatedTime} seconds. Please wait.`;
+  const confirmationMessage = `Please keep this window open during the process. The process will start in approximately ${estimatedTime} seconds. Please wait.`;
   alert(confirmationMessage);
   scrollDownAndRepeat();
 }
