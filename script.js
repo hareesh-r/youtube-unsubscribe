9e14610d185e2ba67ce09373b8b8e02783fd89a8
HAREESH <hareeshprogrammer@gmail.com>
2023-07-09 21:37:23 +0530

    feat: Add functionality to scroll to the bottom, wait, repeat, and scroll to top before executing unsubscribe code

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 let batchCounter = 0;
-let timeout = 150;
+let timeout = 100;
 let scrollHeightOfDiv = document.querySelector("#grid-container > ytd-channel-renderer:nth-child(1)");
 
-async function unsubscribeFromAllChannels() {
+async function scrollDownAndRepeat() {
+  const windowHeight = window.innerHeight;
+
+  for (let i = 0; i < 5; i++) {
+    window.scrollTo(0, document.body.scrollHeight);
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+
+  window.scrollTo(0, 0);
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  executeUnsubscribeCode();
+}
+
+async function executeUnsubscribeCode() {
   const subscriptionButtons = document.querySelectorAll("#notification-preference-button > ytd-subscription-notification-toggle-button-renderer-next > yt-button-shape > button > yt-touch-feedback-shape > div > div.yt-spec-touch-feedback-shape__fill");
 
   for (let i = 0; i < 101; i++) {
     subscriptionButtons[i].click();
     await new Promise(resolve => setTimeout(resolve, timeout));
-    
+
     let unsubscribeOption = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(4) > tp-yt-paper-item");
     if (unsubscribeOption == null) {
       unsubscribeOption = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(2) > tp-yt-paper-item");
@@ -25,9 +38,15 @@ async function unsubscribeFromAllChannels() {
 
     if (i === 100) {
       console.log("Completed batch " + batchCounter++);
-      unsubscribeFromAllChannels();
+      executeUnsubscribeCode();
     }
   }
 }
 
-unsubscribeFromAllChannels();
+async function startUnsubscribeProcess() {
+  window.scrollTo(0, document.body.scrollHeight);
+  await new Promise(resolve => setTimeout(resolve, 2000));
+  scrollDownAndRepeat();
+}
+
+startUnsubscribeProcess();
